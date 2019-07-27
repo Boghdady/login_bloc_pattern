afe8f8233649d56ac0bc8a1a48729b046ca886fd
Ahmed Boghdady <boghdady@Ahmeds-iMac.local>
2019-07-27 15:38:10 +0200

    add undefined view when pass wrong route

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:login_bloc_section14/src/helpers/router.dart';
-import 'package:login_bloc_section14/src/views/screens/products_screen.dart';
 import '../src/helpers/constants.dart';
 
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return (MaterialApp(
+      debugShowCheckedModeBanner: false,
       onGenerateRoute: Router.generateRoute,
       initialRoute: loginRoute,
       title: 'Section 12',
--- lib/src/helpers/router.dart
+++ lib/src/helpers/router.dart
@@ -3,6 +3,7 @@ import 'package:login_bloc_section14/src/models/product.dart';
 import 'package:login_bloc_section14/src/views/screens/login_screen.dart';
 import 'package:login_bloc_section14/src/views/screens/product_details.dart';
 import 'package:login_bloc_section14/src/views/screens/products_screen.dart';
+import 'package:login_bloc_section14/src/views/undefined_view.dart';
 import './constants.dart';
 
 class Router {
@@ -18,9 +19,8 @@ class Router {
             builder: (_) => ProductDetails(product.title, product.qty));
       default:
         return MaterialPageRoute(
-            builder: (_) => Scaffold(
-                  body: Center(
-                      child: Text('No route defined for ${settings.name}')),
+            builder: (context) => UndefinedView(
+                  name: settings.name,
                 ));
     }
   }
